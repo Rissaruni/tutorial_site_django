7e7459ebbb2965a25848f3792b5bb67142890664
Jan Ruth <hidagger.games@gmail.com>
2021-04-15 21:59:03 +0200

    updated documentation

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+"""
+The project level urls.py solves paths up to '/blog' and leads here to process the the remaining parts.
+Remmeber the the variable urlpatterns is a list, so separate elements properly.
+"""
+
 urlpatterns = [
     # views.home calls function we created in views.py
     path('', views.home, name='blog-home'),
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+"""
+Create functions that handle what page the user is supposed to see when using any specific link.
+For now we just return a simple httpresponse.
+"""
+
 
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
